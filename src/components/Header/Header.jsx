1d5ef7bf08e63096ca773613b10c243543721583
Denis Lee <nkoreanarmy@gmail.com>
2021-09-08 11:44:04 +0500

    implement header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NavbarList from "components/NavbarList/NavbarList";
+import Button from "components/Button/Button";
+import styles from "./Header.module.scss";
+
+export default function Header() {
+  return (
+    <header className={[styles.header, "container"].join(" ")}>
+      <NavbarList></NavbarList>
+      <div className={styles.authBtns}>
+        <Button className={styles.btn}>Sign in</Button>
+        <Button className={styles.btn}>Sign up</Button>
+      </div>
+    </header>
+  );
+}
