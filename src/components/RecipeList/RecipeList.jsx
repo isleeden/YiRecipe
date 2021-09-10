8e5ffe5b1d6befbd0b4a96b56f55ff0166158853
Denis Lee <nkoreanarmy@gmail.com>
2021-09-10 15:30:09 +0500

    added title to recipe list

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -2,12 +2,15 @@ import RecipeItem from "components/RecipeListItem/RecipeItem";
 import React from "react";
 import styles from "./RecipeList.module.scss";
 
-export default function RecipeList({ recipes }) {
+export default function RecipeList({ recipes, title }) {
   return (
-    <div className={styles.list}>
-      {recipes.map((recipe) => {
-        return <RecipeItem key={recipe.id} {...recipe} />;
-      })}
+    <div>
+      {title ? <h2 className={styles.title}>{title}</h2> : ""}
+      <div className={styles.list}>
+        {recipes.map((recipe) => {
+          return <RecipeItem key={recipe.id} {...recipe} />;
+        })}
+      </div>
     </div>
   );
 }
