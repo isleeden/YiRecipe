f1a42e5973afe0256d0bd857d5371e5f8558d414
Denis Lee <nkoreanarmy@gmail.com>
2021-09-11 16:41:13 +0500

    add recipe page route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,9 @@
 import Home from "pages/Home";
+import Recipe from "pages/Recipe";
 
-const routes = [{ path: "/", component: Home, exact: true, name: "Home" }];
+const routes = [
+  { path: "/", component: Home, exact: true, name: "Home" },
+  { path: "/recipe", component: Recipe, exact: true, name: "Recipe" },
+];
 
 export default routes;
