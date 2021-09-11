0b2ec82b54956abf9d1a4449fe9364359261afe6
Denis Lee <nkoreanarmy@gmail.com>
2021-09-11 16:39:22 +0500

    implement RecipeImages component

---
+++ src/components/RecipeImages/RecipeImages.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import styles from "./RecipeImages.module.scss";
+
+export default function RecipeImages({ imgs }) {
+  const [currentImg, setCurrentImg] = useState(0);
+
+  return (
+    <div className={styles.recipeImages}>
+      <div className={styles.imgWrapper}>
+        <img src={imgs[currentImg]} alt="recipe_img" />
+      </div>
+      <div className={styles.imgList}>
+        {imgs.map((img, index) => {
+          return (
+            <div className={styles.secImgWrapper}>
+              <img
+                onClick={() => {
+                  setCurrentImg(index);
+                }}
+                src={img}
+                alt="recipe_img"
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
