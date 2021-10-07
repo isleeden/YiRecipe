3fdff30fd1fa42466bce36c0507ac8ae30153fce
Denis Lee <nkoreanarmy@gmail.com>
2021-10-07 15:02:14 +0500

    added logout button for authorised users

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,15 +3,25 @@ import NavbarList from "components/NavbarList/NavbarList";
 import styles from "./Header.module.scss";
 import SignIn from "components/SignIn/SignIn";
 import SignUp from "components/SignUp/SignUp";
+import { useSelector } from "react-redux";
+import LogoutButton from "components/LogoutButton/LogoutButton";
 
 export default function Header() {
+  const isAuth = useSelector((state) => state.user.isAuth);
+
   return (
     <header className={styles.header}>
       <div className={["container", styles.container].join(" ")}>
         <NavbarList></NavbarList>
         <div className={styles.authBtns}>
-          <SignIn></SignIn>
-          <SignUp></SignUp>
+          {isAuth ? (
+            <LogoutButton />
+          ) : (
+            <>
+              <SignIn></SignIn>
+              <SignUp></SignUp>
+            </>
+          )}
         </div>
       </div>
     </header>
