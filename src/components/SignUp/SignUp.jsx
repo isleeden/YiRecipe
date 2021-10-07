e3eeb052af0340c9e51ec1f277dbf34b504bd9fc
Denis Lee <nkoreanarmy@gmail.com>
2021-10-07 11:30:57 +0500

    implement signup submit

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,18 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "components/Input/Input";
-import AuthModal from "components/AuthModal.jsx/AuthModal";
+import AuthModal from "components/AuthModal/AuthModal";
+import { useDispatch } from "react-redux";
+import { signUp } from "store/thunks/userThunks";
 
 export default function SignUp({ isActive, onClose }) {
+  const dispatch = useDispatch();
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const submitHandler = () => {
+    dispatch(signUp(userData));
+  };
+
+  const onEmailChange = (e) => {
+    setUserData({ ...userData, email: e.target.value });
+  };
+
+  const onPasswordChange = (e) => {
+    setUserData({ ...userData, password: e.target.value });
+  };
+
   return (
     <AuthModal
       title="Registration"
       submitText="Sign Up"
       isActive={isActive}
       onClose={onClose}
+      onSubmit={submitHandler}
     >
-      <Input type="text" placeholder="Email" />
+      <Input
+        type="email"
+        placeholder="Email"
+        value={userData.email}
+        onChange={onEmailChange}
+      />
       <Input type="text" placeholder="Personal Name" />
-      <Input type="password" placeholder="Password" />
+      <Input
+        type="password"
+        placeholder="Password"
+        value={userData.password}
+        onChange={onPasswordChange}
+      />
     </AuthModal>
   );
 }
