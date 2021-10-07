f35d5c7cda1957653f495dcd5f553c40db2e255e
Denis Lee <nkoreanarmy@gmail.com>
2021-10-07 11:30:30 +0500

    implement signin submit

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "components/Input/Input";
-import AuthModal from "components/AuthModal.jsx/AuthModal";
+import AuthModal from "components/AuthModal/AuthModal";
+import { useDispatch } from "react-redux";
+import { signIn } from "store/thunks/userThunks";
 
 export default function SignIn({ isActive, onClose }) {
+  const dispatch = useDispatch();
+  const [userData, setUserData] = useState({ email: "", password: "" });
+
+  const submitHandler = () => {
+    console.log(userData);
+    dispatch(signIn(userData));
+  };
+
+  const onEmailChange = (e) => {
+    setUserData({ ...userData, email: e.target.value });
+  };
+
+  const onPasswordChange = (e) => {
+    setUserData({ ...userData, password: e.target.value });
+  };
+
   return (
     <AuthModal
       title="Login"
       submitText="Sign In"
       isActive={isActive}
       onClose={onClose}
+      onSubmit={submitHandler}
     >
-      <Input type="text" placeholder="Email" />
-      <Input type="password" placeholder="Password" />
+      <Input
+        type="email"
+        placeholder="Email"
+        onChange={onEmailChange}
+        value={userData.email}
+      />
+      <Input
+        type="password"
+        placeholder="Password"
+        onChange={onPasswordChange}
+        value={userData.password}
+      />
     </AuthModal>
   );
 }
