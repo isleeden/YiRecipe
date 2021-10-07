bee21ffc04206d33ffcb629225f734a2847a74d5
Denis Lee <nkoreanarmy@gmail.com>
2021-10-07 15:00:25 +0500

    added axios interceptor for refresh token

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+import AuthService from "./AuthService";
 
-const API_URL = "http://localhost:5000/api";
+export const API_URL = "http://localhost:5000/api";
 
 const api = axios.create({
   baseURL: API_URL,
@@ -11,5 +12,21 @@ api.interceptors.request.use((cfg) => {
   cfg.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
   return cfg;
 });
+
+api.interceptors.response.use(
+  (cfg) => {
+    return cfg;
+  },
+  async (err) => {
+    try {
+      if (err.response.status == 401 && err.config && !err.config.isRetry) {
+        err.config.isRetry = true;
+        const response = await AuthService.checkAuth();
+        localStorage.setItem("token", response.data.accessToken);
+        return api.request(err.config);
+      }
+    } catch {}
+  }
+);
 
 export default api;
