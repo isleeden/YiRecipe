5faea58194a1d5cc7095fc6e327503c52ff518cf
Denis Lee <nkoreanarmy@gmail.com>
2021-10-07 11:29:32 +0500

    implement user thunk

---
+++ src/store/thunks/userThunks.js
@@ -0,0 +1,37 @@
+import AuthService from "api/AuthService";
+import { setAuth, setUser } from "store/actionCreators/userActionCreator";
+
+export const signIn = ({ email, password }) => {
+  return async (dispatch) => {
+    try {
+      const response = await AuthService.signIn({ email, password });
+      dispatch(setUser(response.data.user));
+      localStorage.setItem("token", response.data.accessToken);
+      dispatch(setAuth(true));
+    } catch (e) {
+      alert(e.message);
+    }
+  };
+};
+
+export const signUp = ({ email, password }) => {
+  return async (dispatch) => {
+    try {
+      const response = await AuthService.signUp({ email, password });
+      dispatch(setUser(response.data.user));
+      localStorage.setItem("token", response.data.accessToken);
+      dispatch(setAuth(true));
+    } catch {}
+  };
+};
+
+export const logout = () => {
+  return async (dispatch) => {
+    try {
+      await AuthService.logout();
+      dispatch(setUser());
+      localStorage.removeItem("token");
+      dispatch(setAuth(false));
+    } catch {}
+  };
+};
