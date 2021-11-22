0c71b81c7165c2f9decf3d501b0abe8365bacb7b
Denis Lee <nkoreanarmy@gmail.com>
2021-11-22 12:12:32 +0500

    implement fetch data on Home Page replaced mock data with real

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,55 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "components/Header/Header";
 import "styles/pages/Home.scss";
 import RecipeList from "components/RecipeList/RecipeList";
 import SidebarList from "components/SidebarList/SidebarList";
+import RecipeService from "api/RecipeService";
 
 export default function Home() {
-  const recipes = [
-    {
-      id: "0",
-      title: "Spicy Grilled Cheese Sandwich",
-      body: "Grilled cheese with onions, tomatoes and jalapenos-- tastier than the original!",
-      author: "PATELGURL",
-      img: "/images/recipe_1.jpg",
-    },
-    {
-      id: "1",
-      title: "Spicy Grilled Cheese Sandwich",
-      body: "Grilled cheese with onions, tomatoes and jalapenos-- tastier than the original!",
-      author: "PATELGURL",
-      img: "/images/recipe_1.jpg",
-    },
-    {
-      id: "2",
-      title: "Spicy Grilled Cheese Sandwich",
-      body: "Grilled cheese with onions, tomatoes and jalapenos-- tastier than the original!",
-      author: "PATELGURL",
-      img: "/images/recipe_1.jpg",
-    },
-    {
-      id: "3",
-      title: "Spicy Grilled Cheese Sandwich",
-      body: "Grilled cheese with onions, tomatoes and jalapenos-- tastier than the original!",
-      author: "PATELGURL",
-      img: "/images/recipe_1.jpg",
-    },
-    {
-      id: "4",
-      title: "Spicy Grilled Cheese Sandwich",
-      body: "Grilled cheese with onions, tomatoes and jalapenos-- tastier than the original!",
-      author: "PATELGURL",
-      img: "/images/recipe_1.jpg",
-    },
-    {
-      id: "5",
-      title: "Spicy Grilled Cheese Sandwich",
-      body: "Grilled cheese with onions, tomatoes and jalapenos-- tastier than the original!",
-      author: "PATELGURL",
-      img: "/images/recipe_1.jpg",
-    },
-  ];
-
   const recipesSidebar = [
     {
       id: "0",
@@ -74,6 +30,19 @@ export default function Home() {
     },
   ];
 
+  const [recipeState, setRecipeState] = useState([]);
+  const limit = 6;
+  let page = 1;
+
+  const fetchRecipes = async () => {
+    const response = await RecipeService.getAllRecipes(limit, page);
+    setRecipeState(response.data.rows);
+  };
+
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
+
   return (
     <>
       <Header />
@@ -82,7 +51,7 @@ export default function Home() {
           <h1 className="home__title">Home Page</h1>
           <section className="last-added">
             <div className="main-content">
-              <RecipeList recipes={recipes} title="Last Added" />
+              <RecipeList recipes={recipeState} title="Last Added" />
             </div>
             <aside className="sidebar">
               <SidebarList
