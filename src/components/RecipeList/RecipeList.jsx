9a337d80f9b53c34e7edea6e4fd3a6865d3ee0db
Denis Lee <nkoreanarmy@gmail.com>
2021-11-22 12:10:46 +0500

    recipe list: some changes props

--- src/components/RecipeList/RecipeList.jsx
+++ src/components/RecipeList/RecipeList.jsx
@@ -8,7 +8,16 @@ export default function RecipeList({ recipes, title }) {
       {title ? <h2 className={styles.title}>{title}</h2> : ""}
       <div className={styles.list}>
         {recipes.map((recipe) => {
-          return <RecipeItem key={recipe.id} {...recipe} />;
+          return (
+            <RecipeItem
+              id={recipe.id}
+              key={recipe.id}
+              title={recipe.title}
+              body={recipe.body}
+              author={recipe.User}
+              img={recipe.img}
+            />
+          );
         })}
       </div>
     </div>
