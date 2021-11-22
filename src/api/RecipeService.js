6c82203e50b6d84eb42ad5c1ddebd755204f3cbc
Denis Lee <nkoreanarmy@gmail.com>
2021-11-22 12:07:43 +0500

    implement recipe service

--- src/api/RecipeService.js
+++ src/api/RecipeService.js
@@ -1,23 +1,23 @@
 import api from "./index";
 
 export default class RecipeService {
-  static async getAllRecipes() {
-    return await api.get("/recipe");
+  static async getAllRecipes(limit = 10, page = 1) {
+    return await api.get("/recipes", { params: { limit, page } });
   }
 
   static async getRecipe(id) {
-    return await api.get("/recipe/" + id);
+    return await api.get("/recipes/" + id);
   }
 
   static async createRecipe(recipe) {
-    return await api.post("/recipe", { ...recipe });
+    return await api.post("/recipes", recipe);
   }
 
-  static async updateRecipe(id) {
-    return await api.put("/recipe/" + id), { ...recipe };
+  static async updateRecipe(id, recipe) {
+    return await api.put("/recipes/" + id, recipe);
   }
 
   static async deleteRecipe(id) {
-    return await api.delete("/recipe/" + id);
+    return await api.delete("/recipes/" + id);
   }
 }
