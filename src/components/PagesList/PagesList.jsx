7a16122eafb2eb55891456246b349be61e09ca89
Denis Lee <nkoreanarmy@gmail.com>
2021-11-22 15:21:02 +0500

    implement pagination to home page

---
+++ src/components/PagesList/PagesList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./PagesList.module.scss";
+
+export default function PagesList({ pages, onPageClick, currentPage }) {
+  return (
+    <ul className={styles.list}>
+      {pages.map((item) => {
+        return (
+          <li
+            key={item}
+            onClick={onPageClick(item)}
+            className={`${styles.listItem} ${
+              currentPage == item ? styles.active : ""
+            }`}
+          >
+            {item}
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import "styles/pages/Home.scss";
 import RecipeList from "components/RecipeList/RecipeList";
 import SidebarList from "components/SidebarList/SidebarList";
 import RecipeService from "api/RecipeService";
+import PagesList from "components/PagesList/PagesList";
+import { createPages } from "utils/createPages";
 
 export default function Home() {
   const recipesSidebar = [
@@ -31,17 +33,29 @@ export default function Home() {
   ];
 
   const [recipeState, setRecipeState] = useState([]);
+  const [countRecipes, setCountRecipes] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
   const limit = 6;
-  let page = 1;
+  const pagesCount = Math.ceil(countRecipes / limit);
+  const pages = [];
+
+  createPages(pages, pagesCount, currentPage);
 
   const fetchRecipes = async () => {
-    const response = await RecipeService.getAllRecipes(limit, page);
+    const response = await RecipeService.getAllRecipes(limit, currentPage);
     setRecipeState(response.data.rows);
+    setCountRecipes(response.data.count);
   };
 
   useEffect(() => {
     fetchRecipes();
-  }, []);
+  }, [currentPage]);
+
+  const handlePageClick = (item) => {
+    return () => {
+      setCurrentPage(item);
+    };
+  };
 
   return (
     <>
@@ -52,6 +66,13 @@ export default function Home() {
           <section className="last-added">
             <div className="main-content">
               <RecipeList recipes={recipeState} title="Last Added" />
+              <div className="pages-wrapper">
+                <PagesList
+                  pages={pages}
+                  onPageClick={handlePageClick}
+                  currentPage={currentPage}
+                ></PagesList>
+              </div>
             </div>
             <aside className="sidebar">
               <SidebarList
