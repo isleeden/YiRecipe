dc5f5821bf20d0a616656265f2f3477984a5567f
Denis Lee <nkoreanarmy@gmail.com>
2021-11-22 12:09:58 +0500

    added working link to recipe about, recipe item components

--- src/components/RecipeAbout/RecipeAbout.jsx
+++ src/components/RecipeAbout/RecipeAbout.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import styles from "./RecipeAbout.module.scss";
+import { Link } from "react-router-dom";
 
 export default function RecipeAbout({ title, body, author }) {
   return (
     <div className={styles.about}>
       <h1 className={styles.title}>{title}</h1>
       <p className={styles.description}>{body}</p>
-      <a href="/" className={styles.author}>
-        by <span>{author}</span>
-      </a>
+      <Link to={`/users/${author.id}`} className={styles.author}>
+        by <span>{author.name}</span>
+      </Link>
     </div>
   );
 }
--- src/components/RecipeListItem/RecipeItem.jsx
+++ src/components/RecipeListItem/RecipeItem.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import styles from "./RecipeListItem.module.scss";
+import { Link } from "react-router-dom";
 
-export default function RecipeListItem({ title, body, author, img }) {
+export default function RecipeListItem({ id, title, body, author, img }) {
   return (
     <div className={styles.card}>
       <div className={styles.imgWrapper}>
-        <img src={img} alt="" />
+        <img src={img} alt="recipeimg" />
       </div>
       <div className={styles.textCard}>
-        <h3 className={styles.title}>{title}</h3>
+        <h3>
+          <Link className={styles.title} to={`recipes/${id}`}>
+            {title}
+          </Link>
+        </h3>
         <p className={styles.body}>{body}</p>
-        <a href="/" className={styles.author}>
-          By <span>{author}</span>
-        </a>
+        <Link to={`/users/${author.id}`} className={styles.author}>
+          By <span>{author.name}</span>
+        </Link>
       </div>
     </div>
   );
